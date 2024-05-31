5e1592318e7210136cf3c64fa3077606ab42c7a5
Luca Lambiase <luca.lambia@gmail.com>
2024-05-31 12:42:48 +0200

    create basic scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: "App",
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    console.log("App montata");
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader />
+  <h2>Film: </h2>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, illo!</p>
+
+  <h2>Serie: </h2>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, repellat?</p>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    console.log("AppHeader montato");
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>Boolflix Header</h1>
+  </header>
+</template>
+
+<style></style>
